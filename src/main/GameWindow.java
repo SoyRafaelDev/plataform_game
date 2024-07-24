2924cc58a66b21457dd44bad1fb73f5146afa50e
Rafael Gonzalez <130540324+SoyRafaelDev@users.noreply.github.com>
2024-07-25 00:02:12 +0200

    animaciones de RUNNING, IDLE, ATTACK1... se creo el playerSpeed y se crei herencias para modificar las animaciones, aplicando SOLID

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.JFrame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 public class GameWindow {
     private JFrame jframe;
@@ -14,5 +16,17 @@ public class GameWindow {
         jframe.setResizable(false);
         jframe.pack();
         jframe.setVisible(true);
+        jframe.addWindowFocusListener(new WindowFocusListener() {
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                gamePanel.getGame().windowLostFocus();
+            }
+        });
     }
 }
